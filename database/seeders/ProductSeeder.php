21183f45b87d70802138d18ac83dab2d87073f24
mr687 <davinomoehdanino@gmail.com>
2021-10-18 21:15:01 +0700

    :sparkles: added model

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use Illuminate\Database\Seeder;
-
 class ProductSeeder extends Seeder
 {
     /**
@@ -14,6 +13,14 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory(10)->create();
+        // Product::factory(10)->create();
+
+        for ($i=0; $i < 100; $i++) { 
+            Product::create([
+                'name' => "Product ke {$i}",
+                'price' => 10000 * $i,
+                'description' => "Deskripsi product ke {$i}",
+            ]);
+        }
     }
 }
